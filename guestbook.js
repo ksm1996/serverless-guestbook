6c6af19b1c74ea239c28eb064eba9c9d251ec718
ksm1996 <sarpongmensahk@gmail.com>
2022-09-20 19:51:25 +0000

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,13 +1,13 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://eu-gb.functions.appdomain.cloud/api/v1/web/6980164a-c06f-43a9-a0be-16b092492dd0/guestbook2';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}//read-guestbook-entries-sequence2.json`,
       dataType: 'json'
     });
   },
@@ -16,7 +16,7 @@ const guestbook = {
     console.log('Sending', name, email, comment)
     return $.ajax({
       type: 'PUT',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}//save-guestbook-entry-sequence2.json`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         name,
